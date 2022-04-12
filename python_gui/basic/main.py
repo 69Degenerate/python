7d5ef107eaa579be2a3682d79c80e992c7f1f889
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-04-12 21:08:52 +0530

    buttons

--- python_gui/basic/main.py
+++ python_gui/basic/main.py
@@ -1,1 +1,25 @@
-from kivy.app import App
+
+from kivy.app import App as a
+from kivy.uix.button import Button as b
+from kivy.uix.widget import Widget
+
+
+class game(Widget):
+    pass
+# class bu(b):
+#     def __init__(self, **kwargs):
+#         super(bu,self).__init__(**kwargs)
+#         self.text='hello'
+#         self.pos=(15,150)
+#         self.size=(200,200)
+#         self.size_hint=(None,None)
+
+
+class app(a):
+    def build(self):
+        return game()
+
+
+
+if __name__=='__main__':
+    app().run()
