2bfb89f8caa05cfa3103517417626406eb257a76
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-04-13 10:53:09 +0530

    totaly functioning calculator

---
+++ python_gui/calculator/main.py
@@ -0,0 +1,32 @@
+import kivy
+from kivy.app import App
+kivy.require('1.9.0')
+from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', 1)
+
+class CalcGridLayout(GridLayout):
+	def calculate(self, calculation):
+		if calculation:
+			try:
+				# Solve formula and display it in entry
+				# which is pointed at by display
+				self.display.text = str(eval(calculation))
+			except Exception:
+				self.display.text = "Error"
+
+	def delete(self, dele):
+		if dele:
+			try:
+				self.display.text = str(dele[:-1])
+			except Exception:
+				self.display.text = "Error"
+
+
+class CalculatorApp(App):
+	def build(self):
+		return CalcGridLayout()
+        
+calcApp = CalculatorApp()
+calcApp.run()
