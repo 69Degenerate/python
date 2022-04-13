4f82d1765e075718edff571d107c8d1f4b24bccd
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-04-13 10:31:54 +0530

    text box

--- python_gui/basic/main.py
+++ python_gui/basic/main.py
@@ -2,17 +2,11 @@
 from kivy.app import App as a
 from kivy.uix.button import Button as b
 from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
 
 
 class game(Widget):
     pass
-# class bu(b):
-#     def __init__(self, **kwargs):
-#         super(bu,self).__init__(**kwargs)
-#         self.text='hello'
-#         self.pos=(15,150)
-#         self.size=(200,200)
-#         self.size_hint=(None,None)
 
 
 class app(a):
