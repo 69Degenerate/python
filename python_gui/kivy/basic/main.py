892ed1d6746767f86e6a728ef9e8cdf328347fa8
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-04-18 23:15:31 +0530

    img

--- python_gui/kivy/basic/main.py
+++ python_gui/kivy/basic/main.py
@@ -3,6 +3,7 @@ from kivy.app import App as a
 from kivy.uix.button import Button as b
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
+from kivy.core.image import Image
 
 
 class game(Widget):
