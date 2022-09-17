7f720136e87f8936fc177c8dca2e217d4d4f3f4c
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-09-17 20:52:47 +0530

    finalize

---
+++ flask/complete/test.py
@@ -0,0 +1,26 @@
+from  flask import Flask
+from flask import render_template
+import requests
+import json
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route('/contact')
+def link():
+    return render_template("contact.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+    
+@app.route('/services')
+def ser():
+    return render_template("services.html")
+app.run(host="0.0.0.0",port=80)
