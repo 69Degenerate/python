1cd39dc1bc2a418c3ce8244ea18c26e85f2e766d
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-09-18 19:20:57 +0530

    a log in page with psql is done

--- django/login/fpage/views.py
+++ django/login/fpage/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from fpage.models import nlogs
 
 # Create your views here.
-def index(request):
+def home(request):
+
+        # return redirect("")
+    return render(request,'home.html')
+
+def login(request):
     return render(request,'log.html')
+
+
+def index(request):
+    if request.method=="POST":
+            name = request.POST.get('username')
+            p = request.POST.get('password')
+            print(name,' ',p)
+            re=nlogs.objects.filter(uname=p,pas=p)
+            print(re)
+            if re:
+                # return redirect("")
+                return render(request,"home.html")
+            else:
+                # return redirect("home")
+                return render(request,"log.html")
+    else:
+        return render(request,"log.html")
+            
+
--- django/login/login/urls.py
+++ django/login/login/urls.py
@@ -19,5 +19,7 @@ from  fpage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.index, name='login'),
+    path('home',views.home, name='home'),
+    path('index',views.index, name='index')
 ]
