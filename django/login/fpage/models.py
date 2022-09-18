f7e9440f06658d47532c5666be9f648863616ccd
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-09-18 15:56:51 +0530

    login page added

--- django/login/fpage/models.py
+++ django/login/fpage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class nlogs(models.Model):
+    uname = models.CharField(max_length=122)
+    pas = models.CharField(max_length=12)
+    email = models.CharField(max_length=122)
