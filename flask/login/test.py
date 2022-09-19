8e2f856bc505d1d5cadfca9e5d3eff880ec3ce9b
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-09-19 21:56:33 +0530

    input taken from form

--- flask/login/test.py
+++ flask/login/test.py
@@ -2,7 +2,30 @@ from  flask import Flask,request
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/postgres'
 db = SQLAlchemy(app)
 
+
+class logs(db.Model):
+    sno = db.Column(db.String(80), primary_key=True)
+    pas = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+ 
+    
+@app.route("/", methods = ['GET', 'POST'])
+def login():
+    if(request.method=='POST'):
+        u = request.form.get('username')
+        p = request.form.get('password')
+        print(u,' ',p)
+    return render_template("log.html")
+
+
+
+
+
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
