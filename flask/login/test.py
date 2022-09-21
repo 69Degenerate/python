a057f102a3837756abab59f00ea5fb5262f8d33d
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-09-21 23:13:40 +0530

    minor changes

--- flask/login/test.py
+++ flask/login/test.py
@@ -1,6 +1,7 @@
 # from os import uname
+# from curses import flash
 from queue import Empty
-from  flask import Flask,request
+from  flask import Flask,request,flash
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -41,16 +42,17 @@ def create():
         e = request.form.get('email')
         u = request.form.get('username')
         print(u,e,p)
-        s=db.session.execute(db.session.query(logs.uname,logs.pas).where(logs.uname==u,logs.email==e,logs.pas==p)).one_or_none()
+        s=db.session.execute(db.session.query(logs.uname).where(logs.uname==u)).one_or_none()
         print(s)
         if s is None:
-            print('no log')
-            entry = logs(name=u,email = e,pas=p)
-            db.session.add(entry)
-            db.session.commit()
-            return render_template("log.html")
-        else:
             print('log')
+            # entry = logs(name=u,email = e,pas=p)
+            # db.session.add(entry)
+            # db.session.commit()
+            return render_template("create.html")
+        else:
+            print('no log')
+            # flash("sad")
             return render_template("create.html")
     else:
         return render_template("create.html")
