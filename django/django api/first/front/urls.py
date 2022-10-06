7f3ca69a246e5681e9a65557d6b530c2a5dc0969
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-10-06 21:21:44 +0530

    admin section is total api based now

--- django/django api/first/front/urls.py
+++ django/django api/first/front/urls.py
@@ -7,12 +7,4 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('signin',views.signin,name='signin'),
     path('signup',views.signup,name='signup'),
-    path('view',views.view,name='view'),
-    path('add',views.add,name='add'),
-    path('update',views.update,name='update'),
-    path('update1/<int:id>',views.update1,name='update1'),
-    path('update1/',views.update1,name='update1'),
-    path('delete',views.delete,name='delete'),
-    path('delete/<int:id>',views.delete,name='delete1'),
-
 ]
--- django/django api/first/front/views.py
+++ django/django api/first/front/views.py
@@ -23,7 +23,7 @@ def signin(request):
             print(name,' ',p)
             re=logs.objects.filter(uname=name,pas=p)|logs.objects.filter(email=name,pas=p)
             if re:
-                return redirect("view")
+                return redirect("http://127.0.0.1:8000/api/readall")
             else:
                 print(re)
                 messages.warning(request,'user doesnt exist!')
@@ -52,56 +52,6 @@ def signup(request):
             return redirect("signin")
     else:
         return render(request,'signup.html')
-    
-def view(request):
-    books=library.objects.all()
-    context={'books':books}
-    return render(request,'view.html',context)
-
-def add(request):
-    if request.method == 'POST':
-        name = request.POST['book_name']
-        auth = request.POST['author_name']
-        page = request.POST['pages']   
-        new_book=library(book_title=name,book_author=auth,book_pages=page)
-        new_book.save()
-        messages.success(request,'Book added Successfully')
-    return render(request,'add.html')
-
-def update(request):
-    books=library.objects.all()
-    context={'books':books}
-    return render(request,'update.html',context)
-
-def update1(request,id=0):
-    up=library.objects.filter(id=id).first()
-    new={
-        'up':up
-    }
-    if id:
-        book=library.objects.get(id=id)
-        book.delete()
-    if request.method == 'POST':
-            name = request.POST['book_name']
-            auth = request.POST['author_name']
-            page = request.POST['pages']
-            new_book=library(book_title=name,book_author=auth,book_pages=page)
-            new_book.save()
-            return redirect('update')
-    # print(new)
-    return render(request,'update1.html',new)
-
-
-
-def delete(request,id=0):
-    books=library.objects.all()
-    context={'books':books}
-    if id: 
-        book=library.objects.get(id=id)
-        book.delete()
-    return render(request,'delete.html',context)
-
-
 
 
 
