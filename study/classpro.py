39616db4d748d6436c03ac718778ae7e321ac76f
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-10-21 00:03:25 +0530

    learning the oops with python

---
+++ study/classpro.py
@@ -0,0 +1,5 @@
+class demo:
+    s=1
+    d=2
+    def de(self):
+       return self.d+self.s
--- study/py.py
+++ study/py.py
@@ -1,1 +1,106 @@
-print('hola')
+
+# ----------------------------------------------------------------------------------------------------
+# # inheritance
+# class mul:
+#     def m():
+#         print('2*8')
+#     def sub(self,n,m):
+#         return n-m
+
+# class demo:
+#     def add(self,n,m):
+#         return n+m
+
+#     def dadd(self):
+#         return self.add(1,2)
+    
+# class newcls(demo):
+#     pass
+
+# class new(newcls,mul):
+#     pass
+
+# c=demo()
+# print(c.dadd())
+# n=newcls()
+# print(n.dadd)
+# nn=new()
+# print(nn.dadd())
+# mm=new()
+# print(mm.sub(1,1))
+# ----------------------------------------------------------------------------------------------------
+
+
+
+# ----------------------------------------------------------------------------------------------------
+# #  abstraction with python
+# from classpro import demo
+# d= demo()
+# print(d.d,' ',d.s,' ',d.de())
+# ----------------------------------------------------------------------------------------------------
+
+
+
+# ----------------------------------------------------------------------------------------------------
+# # # polymorphism:its means multiple forms
+# # two different classes with same methods 
+# class d:
+#     def one(self):
+#         print('first function')
+#     def sec(self):
+#         print('second fun')
+# class d2:
+#     def one(self):
+#         print('first function')
+#     def sec(self):
+#         print('second fun')
+# q=d()
+# w=d2()
+# q.one()
+# w.one()
+
+# # but these methods can also be different with their operetions
+# class d:
+#     def one(self):
+#         print('first function')
+#     def sec(self):
+#         print('second fun')
+# class d2:
+#     def one(self):
+#         s='first function'
+#         return s
+#     def sec(self):
+#         s='second function'
+#         return s
+# q=d()
+# w=d2()
+# print('first class methods')
+# q.one()
+# q.sec()
+# print('\nsecond class methods')
+# print(w.one())
+# print(w.sec())
+
+# # method overriding
+# class demo:
+#     def one(self):
+#         return 'first original method from parent class'
+# class demo2(demo):
+#     def one(self):
+#         return 'same function but with child class'
+# class demo3(demo):
+#     def one(self):
+#         return 'same function but with second child class'
+# d=demo()
+# d2=demo2()
+# d3=demo3()
+# print('',d.one(),'\n',d2.one(),'\n',d3.one())
+# method overloading is not possible in python
+# ---------------------------------------------------------------------------------------------------
+
+
+
+# ---------------------------------------------------------------------------------------------------
+# encsulation with python
+
+# ---------------------------------------------------------------------------------------------------
