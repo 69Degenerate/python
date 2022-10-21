340586cab8537f808c69d7288d8f3c1aa821f94a
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-10-21 18:44:25 +0530

    failed attempt

---
+++ pattertn.py
@@ -0,0 +1,57 @@
+# for 
+# *
+# **
+# ***
+# ****
+# *****
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print("\r")
+
+# with while
+# n=5
+# i=0
+# while(i<=n):
+#     print(" "*(n-1)+"*"*i)
+#     i=i+1
+
+
+
+# l=[5,1,2,3]
+# for i in l:
+#     print("*"*i)
+
+# m=[[1,1,1],[2,2,2],[3,3,3]]
+# for row in m:
+#     print(row)
+    # for i in row:
+    #     print(i)
+    
+# d={
+#     'n':'dsadw',
+#     'm':'rear'
+# }
+# for i in d:
+#     print(i,':',d.get(i))
+# d['b']='wqe'
+# print(d)
+
+# n=int(input())
+# i=1
+# while i<=10:
+#     print(n,'*',i,'=',n*i)
+#     i+=1
+
+
+
+# m=[[1,2,1],[2,9,2],[2,3,3]]
+# for row in m:
+#     for i in row:
+#         r=0
+#         if r<len(row):
+#             if i==2:
+#                 row[r]=''
+#             r=r+1
+# print(m)
+
--- study/assignment/assingment 2.py
+++
@@ -1,4 +0,0 @@
-#ask two questions :name and favouraite color , then print massage like "jake like red"
-name = input('type name:')
-color = ('favourite color')
-print(name + 'likes' + color)
