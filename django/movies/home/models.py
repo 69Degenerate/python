e705acea66f7b23baf0c300fe76e4cd574809708
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-10-22 14:50:11 +0530

    front page created

--- django/movies/home/models.py
+++ django/movies/home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class moviesinfo(models.Model):
     name=models.CharField(max_length=122)
-    image=models.CharField(max_length=2000)
+    image=models.TextField()
     release_year=models.CharField(max_length=122)
     def __str__(self):
         return self.name
