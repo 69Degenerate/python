6adab68522c9aa71744ae0f5b7ef6438c09b22fa
69Degenerate <kaneki.kenki.1999@gmail.com>
2023-01-04 00:34:21 +0530

    connection test

--- fastapi/demo_mongo/test_demo.py
+++ fastapi/demo_mongo/test_demo.py
@@ -1,17 +1,33 @@
-# import demo
+import demo
 import requests
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 demoname='alll'
+
+def test_connection():
+   client = demo.cli
+   conn=True
+   try:
+      client.admin.command('ismaster')
+   except ConnectionFailure:
+      conn=False
+   assert conn==True
+   
+   
 def test_read():
    res=requests.get("http://127.0.0.1:8000/read")
-   return res.status_code==201
+   assert res.status_code==200
+
 
 def test_readone():
    res=requests.get("http://127.0.0.1:8000/readone/30")
-   return res.status_code==201
+   assert res.status_code==200
+
 
 def test_readnama():
    res=requests.get("http://127.0.0.1:8000/readname/ward")
-   return res.status_code==201
+   assert res.status_code==200
+
 
 def test_items():
    url="http://127.0.0.1:8000/items/"
@@ -26,11 +42,16 @@ def test_items():
       "deptno": 30
          }
    res=requests.post(url,data)
-   return res.status_code==201
+   assert res.status_code==200
+
 
 def test_demo_delete():
    url="http://127.0.0.1:8000/delete/"+demoname
-   return requests.delete(url,timeout=1)
+   res=requests.delete(url)
+   assert res.status_code==200
+
 
 def test_update():
-   res=requests.put
+   n="ward"
+   res=requests.put("http://127.0.0.1:8000/update/?name="+n+"&newname="+n)
+   assert res.status_code==200
