7f88fb56ad30635fd40f6721397b8a3beba83ee0
69Degenerate <kaneki.kenki.1999@gmail.com>
2023-01-03 22:22:03 +0530

    api testing started

--- fastapi/demo_mongo/demo.py
+++ fastapi/demo_mongo/demo.py
@@ -4,13 +4,9 @@ from bson.json_util import dumps,loads
 app = FastAPI()
 cli=pymongo.MongoClient("mongodb://localhost:27017")
 db=cli.workdb.emp
-@app.get("/")
-def root():
-    return {"name": "a"}
-
 
 @app.get("/read")
-def root():
+def read():
     s=db.find({"deptno": 30},{'_id':False})
     print(s)
     # di=[]
@@ -23,13 +19,13 @@ def root():
 @app.get("/readone/{deptno}")
 async def readone(deptno):
     n=int(deptno)
-    s=db.find({"deptno": n},{'_id':False})
-    print(s)
-    di=[]
-    for i in s:
-        di.append(i)
-    print(type(n),n)
-    print(di)
+    # s=db.find({"deptno": n},{'_id':False})
+    # print(s)
+    # di=[]
+    # for i in s:
+    #     di.append(i)
+    # print(type(n),n)
+    # print(di)
     return loads(dumps(list(db.find({"deptno": n},{'_id':False}))))
 
 
@@ -70,10 +66,10 @@ def insert():
 def delete(name):
     db.delete_many({"ename":name})
     s=db.find({"deptno": name},{'_id':False})
-    print(s)
-    di=[]
-    for i in s:
-        di.append(i)
+    # print(s)
+    # di=[]
+    # for i in s:
+    #     di.append(i)
     # return di
     return loads(dumps(list(s)))
 
--- fastapi/demo_mongo/test_demo.py
+++ fastapi/demo_mongo/test_demo.py
@@ -1,2 +1,36 @@
-def test_demo():
-   assert 1+1==2
+# import demo
+import requests
+demoname='alll'
+def test_read():
+   res=requests.get("http://127.0.0.1:8000/read")
+   return res.status_code==201
+
+def test_readone():
+   res=requests.get("http://127.0.0.1:8000/readone/30")
+   return res.status_code==201
+
+def test_readnama():
+   res=requests.get("http://127.0.0.1:8000/readname/ward")
+   return res.status_code==201
+
+def test_items():
+   url="http://127.0.0.1:8000/items/"
+   data={
+      "empno": 7499,
+      "ename": demoname,
+      "job": "salesman",
+      "mgr": 7698,
+      "hiredate": "20-02-81",
+      "sal": 1600,
+      "comm": 300,
+      "deptno": 30
+         }
+   res=requests.post(url,data)
+   return res.status_code==201
+
+def test_demo_delete():
+   url="http://127.0.0.1:8000/delete/"+demoname
+   return requests.delete(url,timeout=1)
+
+def test_update():
+   res=requests.put
