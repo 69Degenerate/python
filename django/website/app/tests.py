d32c44e5feb9473f29b5517ba00cf565a39f4308
69Degenerate <kaneki.kenki.1999@gmail.com>
2023-01-09 21:34:19 +0530

    test struct

--- django/website/app/tests.py
+++ django/website/app/tests.py
@@ -1,22 +1,13 @@
 from django.test import TestCase
-class YourTestClass(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
 
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
-
+class TestUlrls(TestCase):
     def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+        self.assertFalse(True)
 
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
-
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
+class TestModels(TestCase):
+    def test(self):
+        self.assertFalse(True)
+        
+class TestViews(TestCase):
+    def test(self):
+        self.assertFalse(True)
